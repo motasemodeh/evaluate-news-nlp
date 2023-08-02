553d4c25f2a396fbed4271a41d3572731282c556
motasemodeh <motasem.udeh@gmail.com>
2023-08-02 22:22:11 +0300

    adding api key to environment variable

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,12 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express()
 
 app.use(express.static('dist'))
-
 console.log(__dirname)
 
 app.get('/', function (req, res) {
